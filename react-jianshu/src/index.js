66b57d4a936c96cbf8fac47b59ea253b6175ba71
haoxing28 <lihaoxing0827@gmail.com>
2022-01-12 15:17:19 +1100

    init

---
+++ react-jianshu/src/index.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import ReactDOM from 'react-dom';
+import { GlobalStyle } from './style';
+import './style'
+import App from './App';
+
+
+ReactDOM.render(
+  <Fragment>
+    <GlobalStyle/>
+    <App />
+  </Fragment>,
+  document.getElementById('root')
+);
